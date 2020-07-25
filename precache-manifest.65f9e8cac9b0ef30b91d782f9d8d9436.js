c212674653a071ed6016fac4cf1020b2c671f85e
Bohdan <bohdan.makovetskyi@gmail.com>
2020-07-25 15:15:46 +0300

    Updates

---
+++ precache-manifest.65f9e8cac9b0ef30b91d782f9d8d9436.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b73934230ea10ce1f85dd97fd57eac",
+    "url": "/incora_test/index.html"
+  },
+  {
+    "revision": "bb9409fe103adbf7cc7e",
+    "url": "/incora_test/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "db04a26f91963de0e832",
+    "url": "/incora_test/static/css/main.679d6ec1.chunk.css"
+  },
+  {
+    "revision": "bb9409fe103adbf7cc7e",
+    "url": "/incora_test/static/js/2.ac5a0f47.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/incora_test/static/js/2.ac5a0f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db04a26f91963de0e832",
+    "url": "/incora_test/static/js/main.8c9339bb.chunk.js"
+  },
+  {
+    "revision": "e0d0c692f88f4052b82f",
+    "url": "/incora_test/static/js/runtime-main.22522a73.js"
+  },
+  {
+    "revision": "9ec8ee628dc12edaef371f8b1ca725db",
+    "url": "/incora_test/static/media/visa.9ec8ee62.png"
+  }
+]);
